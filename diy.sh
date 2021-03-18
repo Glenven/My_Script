6bf582d81eca60074cc2091da2d6abe4de88305f
h455257166 <heqiming@LERNEN-2020FNPK>
2021-03-18 16:43:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,8 +13,8 @@ author_list="h455257166 dq"
 #获取的脚本来自github加速下载：https://raw.sevencdn.com
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
-scripts_base_url_1=https://raw.sevencdn.com/h455257166/My_Script/JD/
-scripts_base_url_2=https://raw.sevencdn.com/h455257166/My_Script/JD/
+scripts_base_url_1=https://gitee.com/h455257166/My_Script/raw/JD/
+scripts_base_url_2=https://gitee.com/h455257166/My_Script/raw/JD/
 
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.sevencdn.com/SuperManito/JD-FreeFuck/main/
 
