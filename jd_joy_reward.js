e8ae37dc136310b7653e54be5e8c36408a6af0ca
h455257166 <455257166@qq.com>
2021-04-17 16:49:36 +0800

    Update jd_joy_reward.js

--- jd_joy_reward.js
+++ jd_joy_reward.js
@@ -128,7 +128,12 @@ async function joyReward() {
       console.log(`\ndebug场次:${giftSaleInfos}\n`)
       for (let item of data[giftSaleInfos]) {
         console.log(`${item['giftName']}当前库存:${item['leftStock']}`)
-        if (item.giftType === 'jd_bean' && item['giftValue'] === rewardNum) {
+        if (item.giftType === 'jd_bean' && item['giftValue'] && item['leftStock'] > 0) {
+          saleInfoId = item.id;
+          leftStock = item.leftStock;
+          salePrice = item.salePrice;
+          giftValue = item.giftValue;
+        }else if(item.giftType === 'jd_bean' && item['giftValue'] === rewardNum) {
           saleInfoId = item.id;
           leftStock = item.leftStock;
           salePrice = item.salePrice;
