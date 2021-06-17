20533226b35bc97edeb95e078f9038e1c8259aa9
h455257166 <455257166@qq.com>
2021-06-17 11:44:13 +0800

    减少延迟

--- jd_tyt.js
+++ jd_tyt.js
@@ -47,14 +47,10 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
       console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
       if (!$.isLogin) {
         $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
-
-        if ($.isNode()) {
-          await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-        }
         continue
       }
      
-      await $.wait(10000)
+      await $.wait(3000)
       await tythelp()
      
       
