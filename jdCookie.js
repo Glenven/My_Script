c24e92f6c55f8a1d367be38460b09dd514ff480a
h455257166 <455257166@qq.com>
2021-06-17 11:48:01 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -41,8 +41,8 @@ for (let i = 0; i < CookieJDs.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['CookieJD' + index] = CookieJDs[i].trim();
 }
-for (let i = 0; i < CookieNameJDs.length; i++) {
-  if (!CookieNameJDs[i].match(/pt_pin=(.+?);/) || !CookieNameJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieNameJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['CookieNameJD' + index] = CookieNameJDs[i].trim();
-}
+// for (let i = 0; i < CookieNameJDs.length; i++) {
+//   if (!CookieNameJDs[i].match(/pt_pin=(.+?);/) || !CookieNameJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieNameJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);
+//   const index = (i + 1 === 1) ? '' : (i + 1);
+//   exports['CookieNameJD' + index] = CookieNameJDs[i].trim();
+// }
