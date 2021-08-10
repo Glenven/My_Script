4f99473099269927ffa72425de4b166854828d9f
h455257166 <455257166@qq.com>
2021-08-10 20:28:52 +0800

    删除多余脚本

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -154,8 +154,6 @@
 0 0 * * * node /scripts/jd_daydlt.js >> /scripts/logs/jd_daydlt.log 2>&1
 # 金榜创造营，最高瓜分7亿京豆！
 0 8 * * * node /scripts/jd_jbczy.js >> /scripts/logs/jd_jbczy.log 2>&1
-# 东东电竞经理
-# 15 10,20 * * * node /scripts/jd_djjl.js >> /scripts/logs/jd_djjl.log 2>&1
 # 店铺签到
 30,31 1,6,10 * * * node /scripts/jd_dpqd.js >> /scripts/logs/jd_dpqd.log 2>&1
 # 城城领现金
@@ -166,8 +164,7 @@
 
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 # 20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
-# 京小兑
-# 13 8,16,20 * * * node /scripts/jd_jxd.js >> /scripts/logs/jd_jxd.log 2>&1
+
 
 
 ##############京喜APP##############
