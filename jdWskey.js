901a7203e1081897a6895e8f13a58b095f3b4c3a
h455257166 <455257166@qq.com>
2021-09-17 21:28:58 +0800

    fIX

--- jdWskey.js
+++ jdWskey.js
@@ -4,7 +4,7 @@
  */
 //此处填写京东账号wskey。
 let WskeyJDs = [
-'wskey=AAJhHgvwAEBXGqV5kEpV13DDD8P4UM4vWpNosrsm9EFg57l-n_gBBosVLJPi-5PJirTV7ukPZEe137PE8sJT7v2ayVK4wXA6;pin=youhanrong91yvvuj;',
+'',
 '',
 ]
 // 判断环境变量里面是否有京东ck
@@ -17,19 +17,13 @@ if (process.env.JD_WSKEY) {
     WskeyJDs = [process.env.JD_WSKEY];
   }
 }
-if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
-  console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
-  !(async () => {
-    await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
-    await process.exit(0);
-  })()
-}
+
 WskeyJDs = [...new Set(WskeyJDs.filter(item => !!item))]
 console.log(`\n====================共${WskeyJDs.length}个京东账号wskeye=========\n`);
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
 if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
-for (let i = 0; i < WskeyJDs.length; i++) {
-  if (!WskeyJDs[i].match(/pin=(.+?);/) || !WskeyJDs[i].match(/wskey=(.+?);/)) console.log(`\n提示:京东wskey 【${WskeyJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: wskey=xxx;pin=xxx;（分号;不可少）\n`);
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['WskeyJDs' + index] = WskeyJDs[i].trim();
+for (let w = 0; w < WskeyJDs.length; w++) {
+  if (!WskeyJDs[w].match(/pin=(.+?);/) || !WskeyJDs[w].match(/wskey=(.+?);/)) console.log(`\n提示:京东wskey 【${WskeyJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: wskey=xxx;pin=xxx;（分号;不可少）\n`);
+  const index = (w + 1 === 1) ? '' : (w + 1);
+  exports['WskeyJDs' + index] = WskeyJDs[w].trim();
 }
