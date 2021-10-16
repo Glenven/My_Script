6fb9963520b82cbc4cdde66ea019a7d6547f037f
h455257166 <455257166@qq.com>
2021-10-16 14:08:53 +0800

    关闭提醒

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -53,7 +53,7 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*
   if (nowTime > new Date(activeEndTime).getTime()) {
     //活动结束后弹窗提醒
     $.msg($.name, '活动已结束', `该活动累计获得京豆：${$.jingBeanNum}个\n请删除此脚本\n咱江湖再见`);
-    if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `请删除此脚本\n咱江湖再见`);
+    // if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `请删除此脚本\n咱江湖再见`);
     return
   }
   await requireConfig();
