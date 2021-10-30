6f8017c41a01dc7c2ce5fd9f1f9c66c6721cd705
h455257166 <455257166@qq.com>
2021-10-30 15:41:04 +0800

    Update jd_try.js

--- jd_try.js
+++ jd_try.js
@@ -94,11 +94,10 @@ let args_xh = {
                 $.UserName = decodeURIComponent($.cookie.match(/pt_pin=(.+?);/) && $.cookie.match(/pt_pin=(.+?);/)[1])
                 $.index = i + 1;
                 $.isLogin = true;
-                $.nickName = '';
+                $.nickName = cookiename ? cookiename : $.UserName ;
                 await totalBean();
                 console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
                 if(!$.isLogin){
-                    $.nickName = $.cookiename ? $.cookiename : $.UserName ;
                     $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {
                         "open-url": "https://bean.m.jd.com/bean/signIndex.action"
                     });
