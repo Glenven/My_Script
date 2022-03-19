918d6f0981533c4e7e6035cc46d09cdf433416bf
Glenven <1583816394@qq.com>
2022-03-19 14:45:15 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -34,14 +34,14 @@ let newShareCodes=[];
 let message = '', subTitle = '', option = {}, isFruitFinished = false
 const retainWater = $.isNode() ? (process.env.retainWater ? process.env.retainWater : 200) : ($.getdata('retainWater') ? $.getdata('retainWater') : 200);//保留水滴大于多少g,默认200g;
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
-let jdFruitBeanCard = true;//农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
+let jdFruitBeanCard = false;//农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const urlSchema = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://h5.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html%22%20%7D`;
 let NowHour = new Date().getHours();
 let llhelp=true;
 if ($.isNode() && process.env.CC_NOHELPAFTER8) {	
 	if (process.env.CC_NOHELPAFTER8=="true"){
-		if (NowHour>8){
+		if (NowHour>18){
 			llhelp=false;
 			console.log(`现在是9点后时段，不启用互助....`);
 		}			
